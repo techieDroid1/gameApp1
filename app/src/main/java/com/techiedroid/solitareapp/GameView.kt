1f1bf45078e99a04995f4ee1c7ba7a7cc768bf3d
Andy <andy@Andys-MacBook-Pro.local>
2019-02-03 21:14:14 +0530

    ver 1 commit

---
+++ app/src/main/java/com/techiedroid/solitareapp/GameView.kt
@@ -0,0 +1,5 @@
+package com.techiedroid.solitareapp
+
+interface GameView {
+    fun update(gameModel: GameModel = GameModel)
+}
---
+++ app/src/main/java/com/techiedroid/solitareapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.techiedroid.solitareapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val cardWidth = (displayMetrics.widthPixels - dip(8)) / 7
+        val cardHeight = cardWidth * 190 / 140
+
+        verticalLayout {
+            leftPadding = dip(4)
+            rightPadding = dip(4)
+            topPadding = dip(8)
+
+            linearLayout {
+                imageView (imageResource = R.drawable.cardback_green5).lparams(cardWidth, cardHeight)
+                imageView (imageResource = R.drawable.cardback_blue1).lparams(cardWidth, cardHeight)
+                view().lparams(cardWidth, 0)
+
+                for (i in 0..3){
+                    imageView (imageResource = R.drawable.cardback_blue1).lparams(cardWidth, cardHeight)
+                }
+
+            }
+
+            linearLayout {
+
+            }
+        }
+    }
+}
